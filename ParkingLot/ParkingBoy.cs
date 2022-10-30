73b79080372179a8b7b2fdb42872a2595533b894
Bo Zhao <BZhao7@slb.com>
2022-10-30 11:43:05 +0800

    feat(story1-AC1): Return ticket when parking

---
+++ ParkingLot/ParkingBoy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot
+{
+    public class ParkingBoy
+    {
+        private string name;
+        private Parkinglot parkinglot;
+
+        public ParkingBoy(string name, Parkinglot parkinglot)
+        {
+            this.name = name;
+            this.parkinglot = parkinglot;
+        }
+
+        public ParkingTIcket ParkingCar(Car car)
+        {
+            return new ParkingTIcket();
+        }
+    }
+}
---
+++ ParkingLotTest/ParkingBoyTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParkingLot;
+using Xunit;
+
+namespace ParkingLotTest
+{
+    public class ParkingBoyTest
+    {
+        [Fact]
+        public void Should_return_a_ticket_when_parking_given_a_car()
+        {
+            var parkinglot = new Parkinglot("Parkinglot1");
+            var parkingboy = new ParkingBoy("Jacky", parkinglot);
+            var car = new Car("JJAA8888");
+
+            var ticket = parkingboy.ParkingCar(car);
+
+            Assert.IsType<ParkingTIcket>(ticket);
+        }
+    }
+}
